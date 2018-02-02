a1b3d01aaa02a0796cb1b5141cca747893b3e92d
karak <karak97@gmail.com>
2018-02-02 23:12:16 +0900

    Implemented searchWorks by other criteria.

--- src/HtmlClient.ts
+++ src/HtmlClient.ts
@@ -17,10 +17,27 @@ export interface Query {
 
   /** page number, 0-origin */
   page?: number;
+
+  /** name of author that matches with surname or penname */
+  author?: string;
+
+  /** first part(perferct match) */
+  first5?: string;
+
+  /** last part(perferct match) */
+  last5?: string;
+
+  /** free word(partial match) */
+  keyword?: string;
 }
 
 /** Alias to {encodeURIComponent} */
 const enc = encodeURIComponent;
+
+function makeQueryComponent(query: Query, propertyName: string, parameterName?: string) {
+  const actualParameterName = parameterName || propertyName;
+  return propertyName in query ? `&${actualParameterName}=${enc((<any>query)[propertyName])}` : '';
+}
 
 /**
  * Client to retrieve results as HTML.
@@ -34,13 +51,17 @@ export default class HtmlClient {
 
   async searchWorks(query: Query): Promise<string> {
 
-    const kigo = query.kigo ? `&kigo=${enc(query.kigo)}` : '';
+    const kigo = makeQueryComponent(query, 'kigo');
+    const author = makeQueryComponent(query, 'author', 'author_name');
+    const first5 = makeQueryComponent(query, 'first5');
+    const last5 = makeQueryComponent(query, 'last5');
+    const keyword = makeQueryComponent(query, 'keyword');
     const database = `&database=${DB_ENCODED}`;
     const order = '&order=work_up';
     const and = query.page === undefined || query.page === 0 ?
       `submit_and=${SUBMIT_AND_ENCODED}` :
       `search_type=and&next_i=${query.page}`;
-    const queryString = `?${and}${kigo}${database}${order}`;
+    const queryString = `?${and}${kigo}${author}${first5}${last5}${keyword}${database}${order}`;
     const url = `http://${this.host}/search.php${queryString}`;
     return await request.get(url) as string;
   }
