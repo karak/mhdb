c643b4ae3fbb8693727292f4c50a8271906d2f26
karak <karak97@gmail.com>
2018-02-04 05:54:10 +0900

    Added a unit test of HtmlClient to cover completely.

---
+++ __tests__/unit-tests/HtmlClient.test.ts
@@ -0,0 +1,92 @@
+jest.mock('request-promise-native');
+import * as url from 'url';
+import * as qs from 'querystring';
+import * as requestPromise from 'request-promise-native';
+import HtmlClient from '../../src/HtmlClient';
+
+describe('HtmlClient', () => {
+  const HOST = 'localhost/nowhere';
+  const client = new HtmlClient(HOST);
+  const spy = jest.spyOn(requestPromise, 'get');
+
+  describe('searchWorks', () => {
+    it('search by kigo', async () => {
+      await client.searchWorks({ kigo: 'April' });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const queryParams = getQueryParams(spy.mock.calls[0][0]);
+      expect(queryParams['kigo']).toBe('April');
+    });
+
+    it('search by author', async () => {
+      await client.searchWorks({ author: 'Basho Matsuo' });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const queryParams = getQueryParams(spy.mock.calls[0][0]);
+      expect(queryParams['author_name']).toBe('Basho Matsuo');
+    });
+
+    it('search by first5', async () => {
+      await client.searchWorks({ first5: 'ABCDE' });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const queryParams = getQueryParams(spy.mock.calls[0][0]);
+      expect(queryParams['first5']).toBe('ABCDE');
+    });
+
+    it('search by last5', async () => {
+      await client.searchWorks({ last5: 'VWXYZ' });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const queryParams = getQueryParams(spy.mock.calls[0][0]);
+      expect(queryParams['last5']).toBe('VWXYZ');
+    });
+
+    it('search by keyword', async () => {
+      await client.searchWorks({ keyword: 'flower' });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const queryParams = getQueryParams(spy.mock.calls[0][0]);
+      expect(queryParams['keyword']).toBe('flower');
+    });
+
+    describe('page', () => {
+      it('search without page', async () => {
+        await client.searchWorks({});
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        const queryParams = getQueryParams(spy.mock.calls[0][0]);
+        expect(queryParams['submit_and']).toBeDefined();
+      });
+
+      it('search with page 0', async () => {
+        await client.searchWorks({ page: 0 });
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        const queryParams = getQueryParams(spy.mock.calls[0][0]);
+        expect(queryParams['submit_and']).toBeDefined();
+      });
+
+      it('search with page', async () => {
+        await client.searchWorks({ page: 1 });
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        const queryParams = getQueryParams(spy.mock.calls[0][0]);
+        expect(queryParams['submit_and']).not.toBeDefined();
+        expect(queryParams['search_type']).toBe('and');
+        expect(queryParams['next_i']).toBe('1');
+      });
+    });
+  });
+
+  beforeEach(() => {
+    spy.mockReset();
+    spy.mockRestore();
+
+    jest.resetAllMocks();
+  });
+});
+
+function getQueryParams(urlString: string) {
+  return qs.parse(url.parse(urlString).query);
+}
