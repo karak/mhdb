749ad07a92f323ea0bc07264b75cf6ee3fc48d19
karak <karak97@gmail.com>
2018-02-16 21:03:41 +0900

    Set coverage threshold.

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,22 @@ module.exports = {
     "src/**/*.{ts,tsx,js,jsx}",
     "!**/node_modules/**",
   ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 100,
+      "functions": 100,
+      "lines": 90,
+      "statements": 90,
+    },
+    "src/SearchResult.ts": {
+      "statements": 0,
+      "lines": 0,
+    },
+    "src/Work.ts": {
+      "statements": 0,
+      "lines": 0,
+    }
+  },
   "mapCoverage": true
 };
 
