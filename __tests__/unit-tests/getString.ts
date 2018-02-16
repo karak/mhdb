9c6e1954f67f2f17d127bef83dca83d036bfe9a4
karak <karak97@gmail.com>
2018-02-16 20:58:44 +0900

    Added test for getString

---
+++ __tests__/unit-tests/getString.ts
@@ -0,0 +1,12 @@
+import getString from '../../src/getString';
+
+describe('getString', () => {
+  it('can fetch html from "example.com"', async () => {
+    try {
+      const html = await getString('https://example.com');
+      expect(html).toMatch(/<html>.*?<\/html>/);
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
+});
