0d5803e09df4de24efa5e481e42ddc36b88e91cb
karak <karak97@gmail.com>
2018-02-24 20:34:57 +0900

    Added test client.
    [CORS disabled!]

--- __tests__/unit-tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-import search from '../../src/search';
-import * as mhdb from '../../src/index';
-
-describe('index', () => {
-  it('has a member of search', () => {
-    expect(search).toBe(mhdb.search);
-  });
-});
