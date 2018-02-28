b8a4c459f736eb555aa0e0637fb4080ae37ba5ec
karak <karak97@gmail.com>
2018-02-28 15:48:26 +0900

    Applied prettier

--- __tests__/integration-tests/api/HtmlClient.test.ts
+++ __tests__/integration-tests/api/HtmlClient.test.ts
@@ -28,36 +28,39 @@ describe('HtmlClient', () => {
       });
 
       describe('by author(surname or penname)', () => {
-        it ('retrieve works by Basyo', async () => {
+        it('retrieve works by Basyo', async () => {
           const html = await client.searchWorks({ author: '芭蕉' });
           expect(html).toMatch(/古池や蛙飛びこむ水の音/);
         });
       });
 
       describe('by kigo and author(surname or penname)', () => {
-        it ('retrieve works by Basyo', async () => {
-          const html = await client.searchWorks({ kigo: '枯野', author: '芭蕉' });
+        it('retrieve works by Basyo', async () => {
+          const html = await client.searchWorks({
+            kigo: '枯野',
+            author: '芭蕉',
+          });
           expect(html).not.toMatch(/古池や蛙飛びこむ水の音/);
           expect(html).toMatch(/旅に病で夢は枯野をかけ廻る/);
         });
       });
 
       describe('by first5(whole)', async () => {
-        it ('retrieve works that starts with "hirahira-to"', async () => {
+        it('retrieve works that starts with "hirahira-to"', async () => {
           const html = await client.searchWorks({ first5: 'ひらひらと' });
           expect(html).toMatch(/ひらひらと月光降りぬ貝割菜/);
         });
       });
 
       describe('by last5(whole)', async () => {
-        it ('retrieve works that ends with "ari-nu-beshi"', async () => {
+        it('retrieve works that ends with "ari-nu-beshi"', async () => {
           const html = await client.searchWorks({ last5: 'ありぬべし' });
           expect(html).toMatch(/鶏頭の十四五本もありぬべし/);
         });
       });
 
       describe('by keyword', async () => {
-        it ('retrieve works with keyword "thunder"', async () => {
+        it('retrieve works with keyword "thunder"', async () => {
           const html = await client.searchWorks({ keyword: '雷' });
           expect(html).toMatch(/あえかなる薔薇撰りをれば春の雷/);
           expect(html).toMatch(/クリスマス地雷一億地に殘し/);
