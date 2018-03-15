cb52aeb12dac11923f279ebbfdce025fcecb1023
karak <karak97@gmail.com>
2018-03-15 12:59:26 +0900

    Replace 'qs' by standard 'querystring' from Node 6

--- src/api-client/search.ts
+++ src/api-client/search.ts
@@ -2,14 +2,14 @@ import { IncrementalResult } from '../api/search';
 import { Query } from '../api/HtmlClient';
 import Work from '../api/Work';
 import '../polyfill/fetch';
-import * as qs from 'qs';
+import * as qs from 'querystring';
 
 const SEARCH_API_URL = '/search';
 
 export default async function search(
   query: Query,
 ): Promise<IncrementalResult<Work>> {
-  const queryString = qs.stringify(query, { encode: true });
+  const queryString = qs.stringify(query);
   const url = `${SEARCH_API_URL}?${queryString}`;
   return fetch(url, {
     method: 'GET',
